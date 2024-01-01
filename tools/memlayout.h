6ecca477f691e2741695fc6034ddac14b39d0bda
xsxinyi <xsxinyi@outlook.com>
2024-01-01 12:50:42 +0800

    complete entry.S, but still use real address so that can use gdb & vscode to debug

--- tools/memlayout.h
+++ tools/memlayout.h
@@ -25,5 +25,52 @@
 #define USER_CS        ((GD_UTEXT) | DPL_USER)
 #define USER_DS        ((GD_UDATA) | DPL_USER)
 
+
+/* *
+ * Virtual memory map:                                          Permissions
+ *                                                              kernel/user
+ *
+ *     4G ------------------> +---------------------------------+
+ *                            |                                 |
+ *                            |         Empty Memory (*)        |
+ *                            |                                 |
+ *                            +---------------------------------+ 0xFB000000
+ *                            |   Cur. Page Table (Kern, RW)    | RW/-- PTSIZE
+ *     VPT -----------------> +---------------------------------+ 0xFAC00000
+ *                            |        Invalid Memory (*)       | --/--
+ *     KERNTOP -------------> +---------------------------------+ 0xF8000000
+ *                            |                                 |
+ *                            |    Remapped Physical Memory     | RW/-- KMEMSIZE
+ *                            |                                 |
+ *     KERNBASE ------------> +---------------------------------+ 0xC0000000
+ *                            |                                 |
+ *                            |                                 |
+ *                            |                                 |
+ *                            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * (*) Note: The kernel ensures that "Invalid Memory" is *never* mapped.
+ *     "Empty Memory" is normally unmapped, but user programs may map pages
+ *     there if desired.
+ *
+ * */
+
+/* All physical memory mapped at this address */
+// #define KERNBASE            0xC0000000
+
+#define KERNBASE            0x00000000
+#define KMEMSIZE            0x38000000                  // the maximum amount of physical memory
+#define KERNTOP             (KERNBASE + KMEMSIZE)
+
+/* *
+ * Virtual page table. Entry PDX[VPT] in the PD (Page Directory) contains
+ * a pointer to the page directory itself, thereby turning the PD into a page
+ * table, which maps all the PTEs (Page Table Entry) containing the page mappings
+ * for the entire virtual address space into that 4 Meg region starting at VPT.
+ * */
+#define VPT                 0xFAC00000
+
+#define PGSIZE              4096                        // bytes mapped by a page
+#define KSTACKPAGE          2                           // # of pages in kernel stack
+#define KSTACKSIZE          (KSTACKPAGE * PGSIZE)       // sizeof kernel stack
+
 #endif /* !__KERN_MM_MEMLAYOUT_H__ */
 
--- tools/mmu.h
+++ tools/mmu.h
@@ -46,6 +46,19 @@
 #define STS_IG32        0xE            // 32-bit Interrupt Gate
 #define STS_TG32        0xF            // 32-bit Trap Gate
 
+#ifdef __ASSEMBLER__
+
+#define SEG_NULL                                                \
+    .word 0, 0;                                                 \
+    .byte 0, 0, 0, 0
+
+#define SEG_ASM(type,base,lim)                                  \
+    .word (((lim) >> 12) & 0xffff), ((base) & 0xffff);          \
+    .byte (((base) >> 16) & 0xff), (0x90 | (type)),             \
+        (0xC0 | (((lim) >> 28) & 0xf)), (((base) >> 24) & 0xff)
+
+#else /* not __ASSEMBLER__ */
+
 /* Gate descriptors for interrupts and traps */
 struct gatedesc {
     unsigned gd_off_15_0 : 16;        // low 16 bits of offset in segment
@@ -171,6 +184,8 @@ struct taskstate {
     uint16_t ts_t;            // trap on task switch
     uint16_t ts_iomb;        // i/o map base address
 };
+
+#endif /* !__ASSEMBLER__ */
 
 #endif /* !__KERN_MM_MMU_H__ */
 
